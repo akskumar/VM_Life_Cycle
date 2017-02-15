7dea9a59d426026c19b5fa26d1d47d008853c87f
akskumar <aks_0732@yahoo.com>
2017-02-15 22:31:40 +0530

    Contains test cases

---
+++ scripts/VmOperations.py
@@ -0,0 +1,11 @@
+"""
+This module contains all the functions used to manage virtual machine life cycles.
+
+1- Provision a new virtual machine
+2- increase/decrease the Memory of virtual machine
+3- increase/decrease VCPU of virtual machine
+4- Adding/removing HDD in virtual machine
+5- Backup/Recovery of data
+"""
+
+
