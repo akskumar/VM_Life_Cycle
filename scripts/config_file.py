42f9f7b07748f64c6ac6a5804d07be7e2a3c4e70
akskumar <aks_0732@yahoo.com>
2017-02-17 17:48:25 +0530

    Contains test cases

--- scripts/config_file.py
+++ scripts/config_file.py
@@ -3,8 +3,8 @@ This file contains configuration details of mail server and ESXi server
 """
 
 imap_server = "imap.gmail.com"
-username = "kumar.anuj2006"
-password = "Welcome123"
+username = "testanuj64"
+password = "Card#SUB1234"
 ESXi_box = "192.168.174.133"
 ESXi_username = "root"
 ESXi_password = "admin123"
